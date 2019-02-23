510de00d9d2afbb1457e248961de1add4cdea15c
Zach <mail@zachwhittle.com>
2019-02-23 14:05:16 -0600

    implement action mode - to be triggered on initial selection but that's really choppy atm so it's being controlled by the fab

--- app/src/main/java/com/awesome/zach/rvselectmode/MainActivity.kt
+++ app/src/main/java/com/awesome/zach/rvselectmode/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.awesome.zach.rvselectmode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_basic.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SelectableViewHolder.OnItemSelectedListener {
 
+    private var actionModeEnabled = false
+
     private lateinit var adapter: SelectableAdapter
+    private var mActionModeCallback = PrimaryActionModeCallback()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_basic)
+        setSupportActionBar(toolbar)
 
         selection_list.layoutManager = LinearLayoutManager(this)
 
@@ -21,19 +25,55 @@ class MainActivity : AppCompatActivity(), SelectableViewHolder.OnItemSelectedLis
 
         adapter = SelectableAdapter(this, selectableItems, true)
         selection_list.adapter = adapter
+
+        fab.setOnClickListener {
+            if (actionModeEnabled) {
+                finishActionMode()
+            } else {
+                startActionMode()
+            }
+        }
     }
 
-    fun generateItems() : ArrayList<SelectableItem> {
+    private fun startActionMode() {
+        mActionModeCallback.startActionMode(
+            view = toolbar,
+            menuResId = R.menu.menu_action_mode,
+            title = "Title",
+            subtitle = "Subtitle")
+        actionModeEnabled = true
+    }
+
+    private fun finishActionMode() {
+        mActionModeCallback.finishActionMode()
+        actionModeEnabled = false
+    }
+
+    private fun generateItems(): ArrayList<SelectableItem> {
         val selectableItems = ArrayList<SelectableItem>()
         selectableItems.add(SelectableItem("cem", "karaca", false))
-        selectableItems.add(SelectableItem("sezen","aksu", false))
-        selectableItems.add(SelectableItem("baris","manco", false))
+        selectableItems.add(SelectableItem("sezen", "aksu", false))
+        selectableItems.add(SelectableItem("baris", "manco", false))
 
         return selectableItems
     }
 
     override fun onItemSelected(item: SelectableItem) {
         val selectedItems = adapter.getSelectedItems()
-        Snackbar.make(selection_list, "Selected item is ${item.name}. Total selected count is ${selectedItems.size}", Snackbar.LENGTH_LONG).show()
+//        if (selectedItems.isNotEmpty()) {
+//            if (!actionModeEnabled) {
+//                mActionModeCallback.startActionMode(view = toolbar, menuResId = R.menu.menu_action_mode, title = "Title", subtitle = "Subtitle")
+//            }
+//        } else if (selectedItems.isEmpty()) {
+//            if (actionModeEnabled) {
+//                mActionModeCallback.finishActionMode()
+//            }
+//        }
+
+        Snackbar.make(
+            selection_list,
+            "Selected item is ${item.name}. Total selected count is ${selectedItems.size}",
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 }
---
+++ app/src/main/java/com/awesome/zach/rvselectmode/OnActionItemClickListener.kt
@@ -0,0 +1,7 @@
+package com.awesome.zach.rvselectmode
+
+import android.view.MenuItem
+
+interface OnActionItemClickListener {
+    fun onActionItemClick(item: MenuItem)
+}
